391cc64c3e9d3ae1c0e4689e4621698dc298d564
dom <dominicmkennedy@gmail.com>
2024-09-17 11:36:49 -0600

    simple bf interp in python

---
+++ bf.py
@@ -0,0 +1,32 @@
+from sys import argv
+import numpy as np
+
+tape = np.zeros(shape=10000, dtype=np.int64)
+program = open(argv[1], "r").read()
+pairs = {}
+stack = []
+pc = 0
+dp = 0
+for indx, inst in enumerate(program):
+    if inst == "[":
+        stack.append(indx)
+    if inst == "]":
+        pairs[indx] = stack.pop()
+        pairs[pairs[indx]] = indx
+while pc < len(program):
+    match program[pc]:
+        case ">":
+            dp += 1
+        case "<":
+            dp -= 1
+        case "+":
+            tape[dp] += 1
+        case "-":
+            tape[dp] -= 1
+        case ".":
+            print(chr(tape[dp]), end="")
+        case "[":
+            pc = pc if tape[dp] else pairs[pc]
+        case "]":
+            pc = pairs[pc] if tape[dp] else pc
+    pc += 1
