2ad999576f20a46f7025efc45d982a6065b30779
dom <dominicmkennedy@gmail.com>
2024-09-22 15:31:06 -0600

    add timer

--- wasm_runner.js
+++ wasm_runner.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 
 const imports = {
-  'env': { log: x => process.stdout.write(String.fromCharCode(x)) }
+  env: { log: x => process.stdout.write(String.fromCharCode(x)) }
+  // env: { log: x => console.log(x) }
 };
 
-const wasmBuffer = fs.readFileSync('./prog.wasm')
+const wasmBuffer = fs.readFileSync('./rust_prog.wasm')
 WebAssembly.instantiate(wasmBuffer, imports).then(
   results => {
+    console.time("main")
     results.instance.exports.main();
+    console.timeEnd("main")
   }
 )
